3a7c4554a3f55295c852cb488ed9a7544b7b7a69
Saidur289 <saidur.riaz1@gmailcomm>
2025-01-24 01:16:45 +0600

    create some style

--- src/app/AuthProvider.jsx
+++ src/app/AuthProvider.jsx
@@ -1,6 +1,11 @@
 "use client";
 import {KindeProvider} from "@kinde-oss/kinde-auth-nextjs";
-
+const clientId = process.env.KINDE_CLIENT_ID;
+  const domain = process.env.KINDE_ISSUER_URL;
+  console.log(clientId, domain);
 export const AuthProvider = ({children}) => {
-  return <KindeProvider>{children}</KindeProvider>;
+  return <KindeProvider  config={{
+    clientId: clientId,
+    domain: domain,
+  }}>{children}</KindeProvider>;
 };
